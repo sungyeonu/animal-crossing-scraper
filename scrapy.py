3f7f09b4053b54de95e4b2450704cbdb73f7145f
Sungyeon <sungyeonu@gmail.com>
2020-02-24 20:26:14 -0500

    Update scrapy.py

--- scrapy.py
+++ scrapy.py
@@ -1,2 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+
+url = 'https://animalcrossing.fandom.com/wiki/Fish_(New_Horizons)'
+response = (requests.get(url, timeout=5))
+content = BeautifulSoup(response.content, "html.parser").prettify()
+
+print(content)
