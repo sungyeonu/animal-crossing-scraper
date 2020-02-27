006dd967e93e0d890afc32095a8a4be62bbeeea2
Casper Yoo <47223114+sungyeonu@users.noreply.github.com>
2020-02-27 13:42:09 -0500

    Update scrapy.py

--- scrapy.py
+++ scrapy.py
@@ -1,27 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
-
+import io
 urls = {
   # "fish": "https://animalcrossing.fandom.com/wiki/Fish_(New_Horizons)",
   "fish": "https://animalcrossing.fandom.com/wiki/Fish_(New_Leaf)",
   "bugs": "https://animalcrossing.fandom.com/wiki/Bugs_(New_Horizons)"
 }
+fname = "output.txt"
 
 def scrape(key, url): 
   response = (requests.get(url, timeout=5))
   soup = BeautifulSoup(response.content, "html.parser")
   tables = soup.find_all('table', {"class" : "roundy sortable"})
   # tables = soup.find_all('table', attrs={"style" : "background: #76acda"})
-
-  f = open("output.txt",'w')
-  print(tables, file=f)
-
-
+  with io.open(fname, "w", encoding="utf-8") as f:
+    print(tables, file=f)
+    # f.write(tables)
+  
 
 if __name__=="__main__":
   # for key in urls:
   #   scrape(key, urls[key])
-
   scrape("fish", urls["fish"])
 
   
