8456f501613fff0a452c95a637537f24c1d78573
Casper Yoo <47223114+sungyeonu@users.noreply.github.com>
2020-02-27 14:39:59 -0500

    Added test

--- scrapy.py
+++ scrapy.py
@@ -11,11 +11,21 @@ fname = "output.txt"
 def scrape(key, url): 
   response = (requests.get(url, timeout=5))
   soup = BeautifulSoup(response.content, "html.parser")
-  tables = soup.find_all('table', {"class" : "roundy sortable"})
-  # tables = soup.find_all('table', attrs={"style" : "background: #76acda"})
+  itemArr = []
+  itemSoup = soup.find_all('table', {"class" : "roundy sortable"})
+
   with io.open(fname, "w", encoding="utf-8") as f:
-    print(tables, file=f)
-    # f.write(tables)
+    print(itemSoup, file=f)
+
+  with io.open("test.txt", "w", encoding="utf-8") as f1:
+    temp = itemSoup[0]
+
+    for sibling in itemSoup[0].tr.next_siblings:
+      print(repr(sibling), file=f1)
+
+
+
+
   
 
 if __name__=="__main__":
