eab83dbeb8df6a6661e85e507034d3ff4b1ed38f
Casper Yoo <47223114+sungyeonu@users.noreply.github.com>
2020-02-28 23:53:06 -0500

    Update scrapy.py

--- scrapy.py
+++ scrapy.py
@@ -19,9 +19,16 @@ def scrape(key, url):
 
   with io.open("test.txt", "w", encoding="utf-8") as f1:
     temp = itemSoup[0]
-
-    for sibling in itemSoup[0].tr.next_siblings:
+    i = 0
+    for sibling in temp.tr.next_siblings:
+      # if sibling == <td style="background-color:#ffffff; border-bottom: 1px 76acda; border-top-left-radius:10px;-moz-border-radius-topleft:10px;-webkit-border-top-left-radius:10px;-khtml-border-top-left-radius:10px;-icab-border-top-left-radius:10px;-o-border-top-left-radius:10px;border-bottom-left-radius:10px;-moz-border-radius-bottomleft:10px;-webkit-border-bottom-left-radius:10px;-khtml-border-bottom-left-radius:10px;-icab-border-bottom-left-radius:10px;-o-border-bottom-left-radius:10px;"> <a href="/wiki/Bitterling" title="Bitterling">Bitterling</a>
+      # if (sibling.)
+      # print(sibling.name)
+      if (sibling.name == "tr"):
+        i += 1 # there are 72 tr's
+      # print(sibling, file = f1)
       print(repr(sibling), file=f1)
+    print(i)
 
 
 
