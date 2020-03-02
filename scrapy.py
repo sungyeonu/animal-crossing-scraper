5bca8bdaa30a36284f83262b04ded27e7190225c
Sungyeon <sungyeonu@gmail.com>
2020-03-01 23:39:17 -0500

    Finished Fish

--- scrapy.py
+++ scrapy.py
@@ -15,55 +15,36 @@ def scrape(url, output):  # fish
 	itemArr = []
 	itemSoup = soup.find_all("table", {"class": "roundy sortable"})
 
-	with io.open("output.txt", "w", encoding="utf-8") as f1:
-		for item in itemSoup[0].find_all("tr")[11:12]:  # ignore the first element
-			print(item, file=f1)
-			pass
 	with io.open(output, "w", encoding="utf-8") as f:
-		for item in itemSoup[0].find_all("tr")[11:12]:  # ignore the first element
+		for item in itemSoup[0].find_all("tr")[1:]:  # ignore the first element
 			itemInfo = []
-			for td in item.find_all("td", text=True): # maybe look here?
+			for td in item.find_all("td"): # maybe look here?
 				itemInfo.append(td.next.strip())
-				print(td.next.strip(), file=f)
-			# print(itemInfo[0]) # price
-			# print(itemInfo[1]) # loc
-			# print(itemInfo[2]) # size
-			# print(itemInfo[3]) # 1
-			# print(itemInfo[4]) # 2
-			# print(itemInfo[5]) # 3
-			# print("v", itemInfo[6]) # 4 
-			# print("v", itemInfo[7]) # 5 
-			# print("v", itemInfo[8]) # 6 
-			# print("v", itemInfo[9]) # 7
-			# print("v", itemInfo[10]) # 8
-			# print("v", itemInfo[11]) # 9 
-			# print(itemInfo[12]) # 10
-			# print(itemInfo[13]) # 11
 			itemObject = {
 				"name":	item.findChildren("a")[0].text,
 				"image": item.findChildren("a")[1]['href'],
-				"price": itemInfo[0],
-				"location": itemInfo[1],
-				"shadow-size": itemInfo[2], # needs fix
+				"price": itemInfo[2],
+				"location": itemInfo[3],
+				"shadow-size": itemInfo[4],
 				"time": item.findChildren("small")[0].text,
-				"jan": itemInfo[3],
-				"feb": itemInfo[4],
-				"mar": itemInfo[5],
-				"apr": itemInfo[6],
-				"may": itemInfo[7],
-				"jun": itemInfo[8],
-				"jul": itemInfo[9],
-				"aug": itemInfo[10],
-				"sep": itemInfo[11],
-				"oct": itemInfo[12],
-				"nov": itemInfo[13],
-				# "dec": itemInfo[14]
+				"jan": itemInfo[6],
+				"feb": itemInfo[7],
+				"mar": itemInfo[8],
+				"apr": itemInfo[9],
+				"may": itemInfo[10],
+				"jun": itemInfo[11],
+				"jul": itemInfo[12],
+				"aug": itemInfo[13],
+				"sep": itemInfo[14],
+				"oct": itemInfo[15],
+				"nov": itemInfo[16],
+				"dec": itemInfo[17]
 			}
 			itemArr.append(itemObject)
 
 		for i in itemArr:
 			pass
-			# print(i, file=f)
+			print(i, file=f)
 
 if __name__ == "__main__":
 	scrape(urls["fish"], "fish.txt")
