80e0f1e337564d4d275553ec4d2078cc9fa9a751
Sungyeon <sungyeonu@gmail.com>
2020-03-21 21:15:43 -0400

    :rocket:

--- scrapy.py
+++ scrapy.py
@@ -10,35 +10,46 @@ urls = {
     # "bugs": "https://animalcrossing.fandom.com/wiki/Bugs_(New_Leaf)"
 }
 
-
 def scrapeBugs(url):
     response = (requests.get(url, timeout=5))
     soup = BeautifulSoup(response.content, "html.parser")
     itemSoup = soup.find_all("table", {"class": "sortable"})
     itemArr = []
-    for item in itemSoup[0].find_all("tr")[1:]:
+    for item in itemSoup[0].find_all("tr")[3:7]:
+        # print(item)
+    # for item in itemSoup[0].find_all("tr")[1:]:
         itemInfo = []
         for td in item.find_all("td"):
             itemInfo.append(td.next.strip())
         itemObject = {
-            "name":	item.findChildren("a")[0].text,
-            "image": item.findChildren("a")[1]['href'],
+
+            # "imageLink": item.findChildren("a")[1]['href'],
             "price": itemInfo[2],
-            "location": itemInfo[3],
-            "time": item.findChildren("small")[0].text,
-            "jan": itemInfo[5],
-            "feb": itemInfo[6],
-            "mar": itemInfo[7],
-            "apr": itemInfo[8],
-            "may": itemInfo[9],
-            "jun": itemInfo[10],
-            "jul": itemInfo[11],
-            "aug": itemInfo[12],
-            "sep": itemInfo[13],
-            "oct": itemInfo[14],
-            "nov": itemInfo[15],
-            "dec": itemInfo[16]
+            "location": item.findChildren("td")[3].text,
+            # "time": item.findChildren("small")[0].text,
+            # "jan": itemInfo[5],
+            # "feb": itemInfo[6],
+            # "mar": itemInfo[7],
+            # "apr": itemInfo[8],
+            # "may": itemInfo[9],
+            # "jun": itemInfo[10],
+            # "jul": itemInfo[11],
+            # "aug": itemInfo[12],
+            # "sep": itemInfo[13],
+            # "oct": itemInfo[14],
+            # "nov": itemInfo[15],
+            # "dec": itemInfo[16]
         }
+        if (item.findChildren("td")[0].a is None):
+            itemObject["name"] = "Unknown"
+        else:
+            itemObject["name"] = item.findChildren("td")[0].a.text
+        if (item.findChildren("td")[1] is None):
+            itemObject["imageLink"] = "Unknown"
+        else:
+            itemObject["imageLink"] = item.findChildren("td")[1]['href']
+            # itemObject["imageLink"] = item.findChildren("td")[1].text
+
         itemArr.append(itemObject)
     return itemArr
 
@@ -51,14 +62,14 @@ def scrapeFish(url):
         itemInfo = []
         for td in item.find_all("td"):
             itemInfo.append(td.next.strip())
-
         itemObject = {
             "name":	item.findChildren("a")[0].text,
             "imageLink": item.findChildren("a")[1]['href'],
             "price": itemInfo[2],
             "location": item.findChildren("td")[3].text.strip('\n').strip(),
             "shadowSize": itemInfo[4],
-            "time": item.findChildren("small")[0].text,
+            # "time": item.findChildren("small")[0].text, 
+            "time": itemInfo[5], 
             "jan": itemInfo[6],
             "feb": itemInfo[7],
             "mar": itemInfo[8],
@@ -81,6 +92,8 @@ def parseData(itemList, outfile): # turns list to json
     
 if __name__ == "__main__":
     bugsList = scrapeBugs(urls["bugs"])
-    fishList = scrapeFish(urls["fish"])
     parseData(bugsList, "bugs.json")
-    parseData(fishList, "fish.json")
+
+
+    # fishList = scrapeFish(urls["fish"])
+    # parseData(fishList, "fish.json")
