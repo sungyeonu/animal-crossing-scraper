baa823f463f80343cee1c0b26bdeb2bf13980d35
Sungyeon <sungyeonu@gmail.com>
2020-04-30 18:26:06 -0400

    Add north / south hemisphere

--- scrapy.py
+++ scrapy.py
@@ -2,9 +2,8 @@ from bs4 import BeautifulSoup
 import requests, io
 from util import separateByBr, avaiConverter, getPriceWithBellsString, getImageLinks, dumpData
 
-urls = { 
-    # New Horizons
-
+URLS = { 
+    # --- New Horizons ---
     # Critters
     "fish": "https://animalcrossing.fandom.com/wiki/Fish_(New_Horizons)",
     "bugs": "https://animalcrossing.fandom.com/wiki/Bugs_(New_Horizons)",
@@ -22,107 +21,119 @@ urls = {
     "wallMounteds": "https://animalcrossing.fandom.com/wiki/DIY_recipes/Wall-mounted",
     "wallpaperRugsFloorings": "https://animalcrossing.fandom.com/wiki/DIY_recipes/Wallpaper,_rugs_and_flooring",
 
-    # New Leaf
+    # --- New Leaf ---
     # "fish": "https://animalcrossing.fandom.com/wiki/Fish_(New_Leaf)",
-    # "bugs": "https://animalcrossing.fandom.com/wiki/Bugs_(New_Leaf)"
+    # "bugs": "https://animalcrossing.fandom.com/wiki/Bugs_(New_Leaf)",
 }
 
 def scrapeBugs(key): # take url and return object containing bugs data
-    url = urls.get(key)
+    url = URLS.get(key)
     # create soup object
     response = (requests.get(url, timeout=5))
     soup = BeautifulSoup(response.content, "html.parser")
     # find the target table
     table = soup.find_all("table", {"class": "sortable"})
-    # contains all items
-    itemList = []
-    # ignore first row as it just contains labels to the data
+
+    items = {}
+    # go through each tr in the table, ignoring the table header
     for item in table[0].find_all("tr")[1:]:
-        itemInfo = []
+        tableData = []
         # get rid of empty space
         for td in item.find_all("td"):
-            itemInfo.append(td.next.strip())
-
+            tableData.append(td.next.strip())
         # find data and save it into an object
         itemObject = {
-            item.findChildren("td")[0].a.text: {
-                "name": item.findChildren("td")[0].a.text,
-                "imageLink": item.findChildren("a")[1]['href'],
-                "price": int(itemInfo[2]),
-                "location": item.findChildren("td")[3].text.strip('\n').strip(),
-                "time": item.findChildren("small")[0].text,
-                "seasons-northern-hemisphere": {
-                    "jan": avaiConverter(itemInfo[5]),
-                    "feb": avaiConverter(itemInfo[6]),
-                    "mar": avaiConverter(itemInfo[7]),
-                    "apr": avaiConverter(itemInfo[8]),
-                    "may": avaiConverter(itemInfo[9]),
-                    "jun": avaiConverter(itemInfo[10]),
-                    "jul": avaiConverter(itemInfo[11]),
-                    "aug": avaiConverter(itemInfo[12]),
-                    "sep": avaiConverter(itemInfo[13]),
-                    "oct": avaiConverter(itemInfo[14]),
-                    "nov": avaiConverter(itemInfo[15]),
-                    "dec": avaiConverter(itemInfo[16])
-                },
-                "seasons-southern-hemisphere": { # shift northern hemisphere by 6 months
-                    "jan": avaiConverter(itemInfo[11]),
-                    "feb": avaiConverter(itemInfo[12]),
-                    "mar": avaiConverter(itemInfo[13]),
-                    "apr": avaiConverter(itemInfo[14]),
-                    "may": avaiConverter(itemInfo[15]),
-                    "jun": avaiConverter(itemInfo[16]),
-                    "jul": avaiConverter(itemInfo[5]),
-                    "aug": avaiConverter(itemInfo[6]),
-                    "sep": avaiConverter(itemInfo[7]),
-                    "oct": avaiConverter(itemInfo[8]),
-                    "nov": avaiConverter(itemInfo[9]),
-                    "dec": avaiConverter(itemInfo[10])
-                }
+            "name": item.findChildren("td")[0].a.text,
+            "imageLink": item.findChildren("a")[1]['href'],
+            "price": int(tableData[2]),
+            "location": item.findChildren("td")[3].text.strip('\n').strip(),
+            "time": item.findChildren("small")[0].text,
+            "seasons-northern-hemisphere": {
+                "jan": avaiConverter(tableData[5]),
+                "feb": avaiConverter(tableData[6]),
+                "mar": avaiConverter(tableData[7]),
+                "apr": avaiConverter(tableData[8]),
+                "may": avaiConverter(tableData[9]),
+                "jun": avaiConverter(tableData[10]),
+                "jul": avaiConverter(tableData[11]),
+                "aug": avaiConverter(tableData[12]),
+                "sep": avaiConverter(tableData[13]),
+                "oct": avaiConverter(tableData[14]),
+                "nov": avaiConverter(tableData[15]),
+                "dec": avaiConverter(tableData[16])
+            },
+            "seasons-southern-hemisphere": { # shift northern hemisphere by 6 months
+                "jan": avaiConverter(tableData[11]),
+                "feb": avaiConverter(tableData[12]),
+                "mar": avaiConverter(tableData[13]),
+                "apr": avaiConverter(tableData[14]),
+                "may": avaiConverter(tableData[15]),
+                "jun": avaiConverter(tableData[16]),
+                "jul": avaiConverter(tableData[5]),
+                "aug": avaiConverter(tableData[6]),
+                "sep": avaiConverter(tableData[7]),
+                "oct": avaiConverter(tableData[8]),
+                "nov": avaiConverter(tableData[9]),
+                "dec": avaiConverter(tableData[10])
             }
-
         }
-        itemList.append(itemObject)
-    dumpData(itemList, key)
-    return itemList
+        items[item.findChildren("td")[0].a.text] = itemObject
+    dumpData(items, key)
+    # return for debugging
+    return items
 
 def scrapeFish(key): # same logic as scrapeBugs
-    url = urls.get(key)
+    url = URLS.get(key)
     response = (requests.get(url, timeout=5))
     soup = BeautifulSoup(response.content, "html.parser")
     table = soup.find_all("table", {"class": "sortable"})
-    itemList = []
+    items = {}
     for item in table[0].find_all("tr")[1:]:
-        itemInfo = []
+        tableData = []
         for td in item.find_all("td"):
-            itemInfo.append(td.next.strip())
+            tableData.append(td.next.strip())
         itemObject = {
             "name": item.findChildren("a")[0].text,
             "imageLink": item.findChildren("a")[1]['href'],
-            "price": int(itemInfo[2]),
+            "price": int(tableData[2]),
             "location": item.findChildren("td")[3].text.strip('\n').strip(),
-            "shadowSize": itemInfo[4], # specific to fish
+            "shadowSize": tableData[4], # specific to fish
             "time": item.findChildren("small")[0].text, 
-            "jan": avaiConverter(itemInfo[6]),
-            "feb": avaiConverter(itemInfo[7]),
-            "mar": avaiConverter(itemInfo[8]),
-            "apr": avaiConverter(itemInfo[9]),
-            "may": avaiConverter(itemInfo[10]),
-            "jun": avaiConverter(itemInfo[11]),
-            "jul": avaiConverter(itemInfo[12]),
-            "aug": avaiConverter(itemInfo[13]),
-            "sep": avaiConverter(itemInfo[14]),
-            "oct": avaiConverter(itemInfo[15]),
-            "nov": avaiConverter(itemInfo[16]),
-            "dec": avaiConverter(itemInfo[17])
+            "seasons-northern-hemisphere": {
+                "jan": avaiConverter(tableData[6]),
+                "feb": avaiConverter(tableData[7]),
+                "mar": avaiConverter(tableData[8]),
+                "apr": avaiConverter(tableData[9]),
+                "may": avaiConverter(tableData[10]),
+                "jun": avaiConverter(tableData[11]),
+                "jul": avaiConverter(tableData[12]),
+                "aug": avaiConverter(tableData[13]),
+                "sep": avaiConverter(tableData[14]),
+                "oct": avaiConverter(tableData[15]),
+                "nov": avaiConverter(tableData[16]),
+                "dec": avaiConverter(tableData[17])
+            },
+            "seasons-southern-hemisphere": {
+                "jan": avaiConverter(tableData[12]),
+                "feb": avaiConverter(tableData[13]),
+                "mar": avaiConverter(tableData[14]),
+                "apr": avaiConverter(tableData[15]),
+                "may": avaiConverter(tableData[16]),
+                "jun": avaiConverter(tableData[17]),
+                "jul": avaiConverter(tableData[6]),
+                "aug": avaiConverter(tableData[7]),
+                "sep": avaiConverter(tableData[8]),
+                "oct": avaiConverter(tableData[9]),
+                "nov": avaiConverter(tableData[10]),
+                "dec": avaiConverter(tableData[11])
+            }
         }
-        itemList.append(itemObject)
-    dumpData(itemList, key)
-
-    return itemList
+        items[item.findChildren("td")[0].a.text] = itemObject
+    dumpData(items, key)
+    return items
 
 def scrapeFossils(key): # same logic as scrapeBugs and scrapeFish
-    url = urls.get(key)
+    url = URLS.get(key)
     response = (requests.get(url, timeout=5))
     soup = BeautifulSoup(response.content, "html.parser")
     table = soup.find_all("table", {"class": "sortable"})
@@ -162,7 +173,7 @@ def scrapeFossils(key): # same logic as scrapeBugs and scrapeFish
     return itemList
 
 def scrapeVillagers(key):
-    url = urls.get(key)
+    url = URLS.get(key)
     response = (requests.get(url, timeout=5))
     soup = BeautifulSoup(response.content, "html.parser")
     table = soup.find_all("table", {"class": "sortable"})
@@ -181,7 +192,7 @@ def scrapeVillagers(key):
     return itemList
 
 def scrapeDIYTools(key):
-    url = urls.get(key)
+    url = URLS.get(key)
     response = (requests.get(url, timeout=5))
     soup = BeautifulSoup(response.content, "html.parser")
     table = soup.find_all("table", {"class": "sortable"})
@@ -229,7 +240,7 @@ def scrapeDIYTools(key):
     return itemList
 
 def scrapeDIYEquipments(key):
-    url = urls.get(key)
+    url = URLS.get(key)
     response = (requests.get(url, timeout=5))
     soup = BeautifulSoup(response.content, "html.parser")
     table = soup.find_all("table", {"class": "sortable"})
@@ -274,7 +285,7 @@ def scrapeDIYEquipments(key):
 
 
 def scrapeDIYWalls(key):
-    url = urls.get(key)
+    url = URLS.get(key)
     response = (requests.get(url, timeout=5))
     soup = BeautifulSoup(response.content, "html.parser")
     table = soup.find_all("table", {"class": "sortable"})
@@ -301,14 +312,14 @@ def scrapeDIYWalls(key):
 
 if __name__ == "__main__":
     # -- Critters -- 
-    scrapeBugs("bugs")
+    # scrapeBugs("bugs")
     # scrapeFish("fish")
     # scrapeFossils("fossils")
 
-    # # -- Characters -- 
+    # -- Characters -- 
     # scrapeVillagers("villagers")
 
-    # # -- DIY Recipes -- 
+    # -- DIY Recipes -- 
     # scrapeDIYTools("tools")
     # scrapeDIYEquipments("housewares")
     # scrapeDIYEquipments("equipments")
@@ -316,3 +327,4 @@ if __name__ == "__main__":
     # scrapeDIYEquipments("others")
     # scrapeDIYWalls("wallMounteds")
     # scrapeDIYWalls("wallpaperRugsFloorings")
+    pass
